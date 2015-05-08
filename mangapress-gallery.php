5d6e0e737b2554038a33011f2e4adbecbd75c976
Jess Green <jgreen@psy-dreamer.com>
2015-05-08 01:18:03 +0000

    Initial commit

---
+++ mangapress-gallery.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package Manga_Press
+ * @version $Id$
+ * @author Jessica Green <jgreen@psy-dreamer.com>
+ *
+ */
+/*
+ Plugin Name: Gallery for Manga+Pree
+ Plugin URI: http://www.manga-press.com/
+ Description: Adds gallery functionality to mangapress
+ Version: 0.0.0-alpha
+ Author: Jess Green
+ Author URI: http://www.jesgreen.com
+ Text Domain: mangapress-gallery
+ Domain Path: /languages
+*/
